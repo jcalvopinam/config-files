68a5f5c778cb912633edefab32d7c705514478d6
Juan Calvopina <juan.calvopina@gmail.com>
2023-01-24 22:32:06 -0500

    Update keybinding for fzf

--- fzf/fzf-shorcuts.zsh
+++ fzf/fzf-shorcuts.zsh
@@ -1,35 +1,62 @@
-# CTRL-W - Git log
+
+# ---------------------------------------
+# Custom shorcuts
+# ---------------------------------------
+
+# ALT-W - Git log
 fzf-git-log() {
   glogg
 }
-zle     -N            fzf-git-log
-bindkey -M emacs '^W' fzf-git-log
-bindkey -M vicmd '^W' fzf-git-log
-bindkey -M viins '^W' fzf-git-log
+zle     -N             fzf-git-log
+bindkey -M emacs '\ew' fzf-git-log
+bindkey -M vicmd '\ew' fzf-git-log
+bindkey -M viins '\ew' fzf-git-log
 
-# CTRL-B - List the git branches
+
+# CTRL-A - List all git branches
 fzf-git-branch() {
   gbranch
 }
-zle     -N            fzf-git-branch
-bindkey -M emacs '^B' fzf-git-branch
-bindkey -M vicmd '^B' fzf-git-branch
-bindkey -M viins '^B' fzf-git-branch
+zle     -N             fzf-git-branch
+bindkey -M emacs '\ea' fzf-git-branch
+bindkey -M vicmd '\ea' fzf-git-branch
+bindkey -M viins '\ea' fzf-git-branch
 
-# CTRL-S - Git branch switch
+
+# CTRL-E - Git branch switch
 fzf-git-switch() {
   gswitch
 }
-zle     -N            fzf-git-switch
-bindkey -M emacs '^S' fzf-git-switch
-bindkey -M vicmd '^S' fzf-git-switch
-bindkey -M viins '^S' fzf-git-switch
+zle     -N             fzf-git-switch
+bindkey -M emacs '\ee' fzf-git-switch
+bindkey -M vicmd '\ee' fzf-git-switch
+bindkey -M viins '\ee' fzf-git-switch
+
 
 # CTRL-D - Git diff
 fzf-git-diff() {
   gdiff
 }
-zle     -N            fzf-git-diff
-bindkey -M emacs '^D' fzf-git-diff
-bindkey -M vicmd '^D' fzf-git-diff
-bindkey -M viins '^D' fzf-git-diff
+zle     -N             fzf-git-diff
+bindkey -M emacs '\ed' fzf-git-diff
+bindkey -M vicmd '\ed' fzf-git-diff
+bindkey -M viins '\ed' fzf-git-diff
+
+# ALT-S - Git log
+fzf-git-status() {
+  gstatus
+}
+zle     -N             fzf-git-status
+bindkey -M emacs '\es' fzf-git-status
+bindkey -M vicmd '\es' fzf-git-status
+bindkey -M viins '\es' fzf-git-status
+
+# ALT-P - Git parent
+fzf-git-parent() {
+  gparent
+}
+zle     -N             fzf-git-parent
+bindkey -M emacs '\er' fzf-git-parent
+bindkey -M vicmd '\er' fzf-git-parent
+bindkey -M viins '\er' fzf-git-parent
+
