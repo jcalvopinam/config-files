ce77608119e248d09e1a06fc5cbddb7352661f6a
Juan Calvopina <jcalvopinam@gmail.com>
2026-07-04 19:41:35 -0500

    Update the color of the Apple icon

--- p10k/.p10k.zsh
+++ p10k/.p10k.zsh
@@ -190,7 +190,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=255
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=#57606A
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
